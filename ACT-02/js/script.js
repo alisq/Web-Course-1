a627f1b2ce81d434df3f337165db679ea8bb65c8
alisq <ali.shamas.qadeer@gmail.com>
2019-10-02 10:52:01 -0400

    updated

--- ACT-02/js/script.js
+++ ACT-02/js/script.js
@@ -2,7 +2,7 @@
 
 
 var students = ["Lina Akkawi", "Khes Bowen", "Karlo Calayag", "Sophia Choi", "Adrienne Dacoco", "Yasmin Emery", "Katherine Garnett", "Oleg Gorlenko", "Jaywhy Kim", "Hwa Koo Seung", "Siwoo Lee", "Justine Ly", "Andrea Mejia Neri", "Kyle Miron", "Alleesa Paragas", "Oanh Pham", "Sabrina Singh", "Su Tarhan", "Jillian Taverner", "Allison Truong", "Cole Wielgosz", "Christine Xia"]
-var actions = ["Random", "Click", "Hover", "Show/Hide", "Cursor", "Scroll", "Type", "Drag", "Random", "Click", "Hover", "Show/Hide", "Cursor", "Scroll", "Type", "Drag", "Drag", "Scroll", "Type", "Cursor", "Hover", "Click", "Show/Hide", "Random", "Drag", "Scroll"]
+var actions = ["Random", "Click", "Hover", "Show/Hide", "Cursor", "Scroll", "Type", "Drag", "Random", "Click", "Hover", "Show/Hide", "Cursor", "Scroll", "Type", "Drag", "Scroll", "Type", "Cursor", "Hover", "Click", "Show/Hide", "Random", "Drag", "Scroll"]
 
 
 for (var i=0; i<students.length;i++) {
